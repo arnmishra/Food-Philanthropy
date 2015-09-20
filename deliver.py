0d7acc6f59fd24aafc777ab5d3c0e50050c1190e
Arnav Mishra <arnavmishra@Arnavs-MacBook-Pro.local>
2015-09-20 10:40:07 -0400

    Adding flask

--- deliver.py
+++ deliver.py
@@ -8,7 +8,7 @@ import sys
 
 init()
 
-def delivers(name, number, description):
+def delivers(company_name, phone, description, start_address, end_address, end_name):
     """
     try:
         os.environ["POSTMATES_KEY"] = "a254dd7a-a9ba-4e30-9adf-7e72781caba3" #TAKE OUT
@@ -27,18 +27,26 @@ def delivers(name, number, description):
 
     postmate = pm.PostmatesAPI("a254dd7a-a9ba-4e30-9adf-7e72781caba3", "cus_KUqGApvB2kLJsF")
 
-    print str(postmate)
+    delivery = order(postmate, name, number, description, start_address, end_address, end_name)
 
-    delivery = order(postmate, name, number, description)
+    delivery.update_status()
+    get_status(delivery)
 
+    fixed_file = open("end.html", "a")
+    fixed_file.write("Name: " + name)
+    fixed_file.write("Number: " + number)
+    fixed_file.write("Description: " + description)
+    fixed_file.write("Pick address: " + start_address)
+    fixed_file.write("Delivery address: " + end_address)
+    fixed_file.write("Delivery name: " + end_name)
+    fixed_file.close()
+
+    '''
     interact = True
-
-    print "hello"
 
     while(interact):
 
         #command = raw_input("Status, Cancel, Done: ")
-        print "hi"
         command = "Done"
 
         if(command == "Status"): #check what button the user presses
@@ -59,6 +67,8 @@ def delivers(name, number, description):
 
         else:
             print "Ya dun messed up. Put in a real command pls."
+    '''
+    fix_file(delivery)
 
 def get_status(delivery):
     if(delivery.status == "pending"):
@@ -72,22 +82,24 @@ def get_status(delivery):
     else:
         print "Something is wrong PLS FIX STATUS"
 
-def order(postmate, name, number, manifest):
+def order(postmate, name, number, manifest, start_address, end_address, end_name):
 
     #pickup =  Get the location from Saketh's part.
-    pickup = "77 Massachusetts Ave, Cambridge, MA 02139"
+    #pickup = "77 Massachusetts Ave, Cambridge, MA 02139"
 
     #dropoff = Get array with name, address, number from Katyayni's part
-    end = ["Bob", "Massachusetts Hall Cambridge, MA 02138", "415-777-9999"]    
+    #end = ["Bob", "Massachusetts Hall Cambridge, MA 02138", "415-777-9999"]    
 
     # This info should come in via parameters from the form on the website
     #name = raw_input("Please enter your company's name: ")
     #number = raw_input("Please enter your Phone number (i.e. 123-456-7890): ")
     #manifest = raw_input("Please enter what you will be i (i.e. 50 bagels): ")
 
-    pickup = pm.Location(name, pickup, number)
+    phone = "408-666-0764"
 
-    dropoff = pm.Location(end[0], end[1], end[2])
+    pickup = pm.Location(name, start_address, number)
+
+    dropoff = pm.Location(end_name, phone, end_address)
 
     delivery = pm.Delivery(postmate, manifest, pickup, dropoff)
     
